0aaae5c8eab34b5e3fd3e30d4938ac4360884dfd
supradipm787 <supradipm7@gmail.com>
2020-05-19 22:33:34 +0530

    First Commit

---
+++ spring-boot-microservice-forex-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','INR',72,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','INR',79,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'POUND','INR',85,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10004,'AUD','INR',48,0);
